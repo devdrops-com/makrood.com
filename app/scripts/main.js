b2ed2559c7af2b6bd8532ec19f310f8f6d488292
shin@nawagate <shin@nawagate.com>
2016-08-08 15:59:55 +0900

    no message

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,69 +1,72 @@
 (function() {
   'use strict';
-  var $body = $('body');
   // スクロールスパイ
-  $body.scrollspy({target: '.navbar-right', offset: 100});
-  // スムーススクロール
-  $('.navbar-scroll a').smoothScroll({
-    beforeScroll: function() {
-      $body.addClass('smooth-scroll-scrolling');
-      $('.navbar-collapse.collapse.in').collapse('hide');
-    },
-    afterScroll: function() {
-      $body.removeClass('smooth-scroll-scrolling');
-    }
-  });
-  // ニュースのサムネイルの高さを揃える
-  if ($(window).width() >= 768) {
-    $('#top-news [class^="col-"] > a').tile(4);
-  }
-  // このページの先頭に戻る
-  $(window).scroll(function() {
-    if ($(window).scrollTop() > 100) {
-      $('#return-pagetop').fadeIn('slow');
-    } else {
-      $('#return-pagetop').fadeOut('slow');
-    }
-  });
-  $('#mainvisual-eyecatch-slider').slick({fade: true, arrows: false});
+  $('body').scrollspy({target: '#global-navigation', offset: 100});
+  // メインビジュアル スライダー
+  $('#mainvisual-eyecatch-slider').slick({autoplay: true, autoplaySpeed: 3000, fade: true, pauseOnHover: false, speed: 3000});
+  // メインビジュアル スライダー min-height設定
   $('#mainvisual-eyecatch-slider .mainvisual-eyecatch-slider-image').css('min-height', $(window).height());
-  function initialize() {
-    var latlng = new google.maps.LatLng(31.571655, 130.551444);
-    var myOptions = {
-      zoom: 18,
-      center: latlng,
-      mapTypeControlOptions: {
-        mapTypeIds: ['sample', google.maps.MapTypeId.ROADMAP]
-      }
-    };
-    var map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
-    var icon = new google.maps.MarkerImage('../images/map-icon.png', new google.maps.Size(55, 72), new google.maps.Point(0, 0));
-    var markerOptions = {
-      position: latlng,
-      map: map,
-      icon: icon,
-      title: '株式会社Lig'
-    };
-    var marker = new google.maps.Marker(markerOptions);
-    var styleOptions = [
-      {
-        ' stylers': [
-          {
-            ' saturation': -100
-          }, {
-            ' visibility': ' simplified'
-          }, {
-            ' lightness': 22
-          }
-        ]
-      }
-    ];
-    var styledMapOptions = {
-      name: 'マックルー'
-    }
-    var sampleType = new google.maps.StyledMapType(styleOptions, styledMapOptions);
-    map.mapTypes.set('sample', sampleType);
-    map.setMapTypeId('sample');
-  }
-  google.maps.event.addDomListener(window, 'load', initialize);
+
+  // var $body = ;
+  // // スムーススクロール
+  // $('.navbar-scroll a').smoothScroll({
+  //   beforeScroll: function() {
+  //     $body.addClass('smooth-scroll-scrolling');
+  //     $('.navbar-collapse.collapse.in').collapse('hide');
+  //   },
+  //   afterScroll: function() {
+  //     $body.removeClass('smooth-scroll-scrolling');
+  //   }
+  // });
+  // // ニュースのサムネイルの高さを揃える
+  // if ($(window).width() >= 768) {
+  //   $('#top-news [class^="col-"] > a').tile(4);
+  // }
+  // // このページの先頭に戻る
+  // $(window).scroll(function() {
+  //   if ($(window).scrollTop() > 100) {
+  //     $('#return-pagetop').fadeIn('slow');
+  //   } else {
+  //     $('#return-pagetop').fadeOut('slow');
+  //   }
+  // });
+  // function initialize() {
+  //   var latlng = new google.maps.LatLng(31.571655, 130.551444);
+  //   var myOptions = {
+  //     zoom: 18,
+  //     center: latlng,
+  //     mapTypeControlOptions: {
+  //       mapTypeIds: ['sample', google.maps.MapTypeId.ROADMAP]
+  //     }
+  //   };
+  //   var map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
+  //   var icon = new google.maps.MarkerImage('../images/map-icon.png', new google.maps.Size(55, 72), new google.maps.Point(0, 0));
+  //   var markerOptions = {
+  //     position: latlng,
+  //     map: map,
+  //     icon: icon,
+  //     title: '株式会社Lig'
+  //   };
+  //   var marker = new google.maps.Marker(markerOptions);
+  //   var styleOptions = [
+  //     {
+  //       ' stylers': [
+  //         {
+  //           ' saturation': -100
+  //         }, {
+  //           ' visibility': ' simplified'
+  //         }, {
+  //           ' lightness': 22
+  //         }
+  //       ]
+  //     }
+  //   ];
+  //   var styledMapOptions = {
+  //     name: 'マックルー'
+  //   }
+  //   var sampleType = new google.maps.StyledMapType(styleOptions, styledMapOptions);
+  //   map.mapTypes.set('sample', sampleType);
+  //   map.setMapTypeId('sample');
+  // }
+  // google.maps.event.addDomListener(window, 'load', initialize);
 })();
