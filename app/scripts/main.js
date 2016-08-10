c18aa79124ec594c4a7697a480975c705e1ce0a8
shin@nawagate <shin@nawagate.com>
2016-08-10 18:26:41 +0900

    no message

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,10 +3,16 @@
   // スクロールスパイ
   $('body').scrollspy({target: '#global-navigation', offset: 100});
   // メインビジュアル スライダー
-  $('#mainvisual-eyecatch-slider').slick({autoplay: true, autoplaySpeed: 3000, fade: true, pauseOnHover: false, speed: 3000});
+  $('#mainvisual-eyecatch-slider').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    fade: true,
+    pauseOnHover: false,
+    speed: 3000
+  });
   // メインビジュアル スライダー min-height設定
   $('#mainvisual-eyecatch-slider .mainvisual-eyecatch-slider-image').css('min-height', $(window).height());
-
   // var $body = ;
   // // スムーススクロール
   // $('.navbar-scroll a').smoothScroll({
@@ -20,7 +26,7 @@
   // });
   // // ニュースのサムネイルの高さを揃える
   // if ($(window).width() >= 768) {
-  //   $('#top-news [class^="col-"] > a').tile(4);
+  //   $('#top-news [class^='col-'] > a').tile(4);
   // }
   // // このページの先頭に戻る
   // $(window).scroll(function() {
@@ -30,43 +36,43 @@
   //     $('#return-pagetop').fadeOut('slow');
   //   }
   // });
-  // function initialize() {
-  //   var latlng = new google.maps.LatLng(31.571655, 130.551444);
-  //   var myOptions = {
-  //     zoom: 18,
-  //     center: latlng,
-  //     mapTypeControlOptions: {
-  //       mapTypeIds: ['sample', google.maps.MapTypeId.ROADMAP]
-  //     }
-  //   };
-  //   var map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
-  //   var icon = new google.maps.MarkerImage('../images/map-icon.png', new google.maps.Size(55, 72), new google.maps.Point(0, 0));
-  //   var markerOptions = {
-  //     position: latlng,
-  //     map: map,
-  //     icon: icon,
-  //     title: '株式会社Lig'
-  //   };
-  //   var marker = new google.maps.Marker(markerOptions);
-  //   var styleOptions = [
-  //     {
-  //       ' stylers': [
-  //         {
-  //           ' saturation': -100
-  //         }, {
-  //           ' visibility': ' simplified'
-  //         }, {
-  //           ' lightness': 22
-  //         }
-  //       ]
-  //     }
-  //   ];
-  //   var styledMapOptions = {
-  //     name: 'マックルー'
-  //   }
-  //   var sampleType = new google.maps.StyledMapType(styleOptions, styledMapOptions);
-  //   map.mapTypes.set('sample', sampleType);
-  //   map.setMapTypeId('sample');
-  // }
-  // google.maps.event.addDomListener(window, 'load', initialize);
+  function initialize() {
+    var latlng = new google.maps.LatLng(31.571655, 130.551444);
+    var myOptions = {
+      zoom: 18,
+      center: latlng,
+      scrollwheel: false,
+      mapTypeControlOptions: {
+        mapTypeIds: ['sample', google.maps.MapTypeId.ROADMAP]
+      }
+    };
+    var map = new google.maps.Map(document.getElementById('access-map'), myOptions);
+    var icon = new google.maps.MarkerImage('../images/map-icon.png', new google.maps.Size(80, 85), new google.maps.Point(0, 0));
+    var markerOptions = {
+      position: latlng,
+      map: map,
+      icon: icon,
+      title: 'Herbal Shop Makrood'
+    };
+    var marker = new google.maps.Marker(markerOptions);
+    // スタイル
+    var styleOptions = [
+      {
+        'stylers': [
+          {
+            'hue': '#e6e400'
+          }, {
+            'visibility': 'simplified'
+          }
+        ]
+      }
+    ];
+    var styledMapOptions = {
+      name: 'Herbal Shop Makrood'
+    }
+    var sampleType = new google.maps.StyledMapType(styleOptions, styledMapOptions);
+    map.mapTypes.set('sample', sampleType);
+    map.setMapTypeId('sample');
+  }
+  google.maps.event.addDomListener(window, 'load', initialize);
 })();
